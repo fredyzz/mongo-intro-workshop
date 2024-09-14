b14063e441569f5be3ed1458edc74f3df6e62574
Alfredo Zimperz <17734680+fredyzz@users.noreply.github.com>
2024-09-14 08:47:36 +0200

    1.Connect to ddbb and save a document

---
+++ test.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const connect = () => {
+  // Here we should specify the connection string whit this format:
+  // protocoll://<dbuser>:<dbpassword>@ds123456.mlab.com:23456/dbname
+  return mongoose.connect("mongodb://localhost:27017/testdb");
+};
+
+// 1. Define the schema
+const studentSchema = new mongoose.Schema({
+  firstName: String,
+});
+
+// 2. Define the model - with this model mongo will create a collection called "student"
+const Student = mongoose.model("student", studentSchema);
+
+async function main() {
+  try {
+    // 3. Connect to the database
+    const connection = await connect();
+    console.log("Connected to the database", { connection });
+
+    // 4. Create a new student in ddbb
+    const student = await Student.create({ firstName: "John" });
+    console.log(student);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+main();
